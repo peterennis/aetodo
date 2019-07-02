09e4cc7f9f0a999f5ab06958e4e1fe63bf456ed1
peterennis <peterennis@yahoo.com>
2019-07-01 20:33:50 -0700

    fix(revert): back to start with aepwa v0.1.0

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,48 +1,28 @@
-import { Component, State, h } from '@stencil/core';
-import { Aetodo } from "../../interfaces/aetodo";
+import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'app-home',
   styleUrl: 'app-home.css'
 })
 export class AppHome {
-  @State() todos: Aetodo[] = [];
-
-  componentWilLoad() {
-    this.todos = [
-      { id: "1", title: "first todo", description: "the first thing todo" },
-      { id: "2", title: "second todo", description: "the second thing todo" },
-      { id: "3", title: "third todo", description: "the third thing todo" },
-      { id: "4", title: "fourth todo", description: "the fourth thing todo" },
-      { id: "5", title: "fifth todo", description: "the fifth thing todo" }
-    ];
-    console.log(this.todos)
-  }
 
   render() {
     return [
       <ion-header>
         <ion-toolbar color="primary">
-          <ion-title>Todos</ion-title>
-          <ion-buttons slot="end">
-            <ion-button>
-              <ion-icon slot="icon-only" name="add"></ion-icon>
-            </ion-button>
-          </ion-buttons>
+          <ion-title>Home</ion-title>
         </ion-toolbar>
       </ion-header>,
 
       <ion-content class="ion-padding">
-        <ion-list lines="none">
-          {this.todos.map(todo => (
-            <div class="aetodo-container">
-              <ion-checkbox />
-              <ion-item button href="">
-                <ion-label>test</ion-label>
-              </ion-item>
-            </div>
-          ))}
-        </ion-list>
+        <p>
+          Welcome to the PWA Toolkit. You can use this starter to build entire
+          apps with web components using Stencil and ionic/core! Check out the
+          README for everything that comes in this starter out of the box and
+          check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
+        </p>
+
+        <ion-button href="/profile/ionic" expand="block">Profile page</ion-button>
       </ion-content>
     ];
   }
