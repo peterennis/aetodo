4f20e4593ac04308c40a3624abe884181a17188a
peterennis <peterennis@yahoo.com>
2019-07-01 19:18:08 -0700

    chore(): create sample todos

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,10 +1,23 @@
-import { Component, h } from '@stencil/core';
+import { Component, State, h } from '@stencil/core';
+import { Aetodo } from "../../interfaces/aetodo";
 
 @Component({
   tag: 'app-home',
   styleUrl: 'app-home.css'
 })
 export class AppHome {
+  @State() todos: Aetodo[] = [];
+
+  componentWilLoad() {
+    this.todos = [
+      { id: "1", title: "first todo", description: "the first thing todo" },
+      { id: "2", title: "second todo", description: "the second thing todo" },
+      { id: "3", title: "third todo", description: "the third thing todo" },
+      { id: "4", title: "fourth todo", description: "the fourth thing todo" },
+      { id: "5", title: "fifth todo", description: "the fifth thing todo" }
+    ];
+    console.log(this.todos)
+  }
 
   render() {
     return [
