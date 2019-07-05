731e24cc90ff1ffd4ee28857f313e5b9dc0be8e0
peterennis <peterennis@yahoo.com>
2019-07-04 19:37:47 -0700

    fix(): icon button add

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -24,19 +24,17 @@ export class AppHome {
     return [
       <ion-header>
         <ion-toolbar color="primary">
-          <ion-title>Home</ion-title>
+          <ion-title>Todos</ion-title>
+          <ion-buttons slot="end">
+            <ion-button>
+              <ion-icon slot="icon-only" name="add"></ion-icon>
+            </ion-button>
+          </ion-buttons>
         </ion-toolbar>
       </ion-header>,
 
       <ion-content class="ion-padding">
-        <p>
-          Welcome to the PWA Toolkit. You can use this starter to build entire
-          apps with web components using Stencil and ionic/core! Check out the
-          README for everything that comes in this starter out of the box and
-          check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
-        </p>
 
-        <ion-button href="/detail/ionic" expand="block">Detail page</ion-button>
       </ion-content>
     ];
   }
