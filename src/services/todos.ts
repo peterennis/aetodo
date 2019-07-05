eec917b80bebfcbaabf9da36d51364a6e3057dd6
peterennis <peterennis@yahoo.com>
2019-07-04 20:19:20 -0700

    feat(): setup skeleton for todo methods

--- src/services/todos.ts
+++ src/services/todos.ts
@@ -1,7 +1,34 @@
+import { Todo } from "../services/todo";
+
 class TodoController {
+
+  private todos: Todo[];
 
   constructor() { }
 
+  load(): Promise<Todo[]> {
+
+  }
+
+  save(): Promise<void> {
+
+  }
+
+  getTodos(): Promise<Todo[]> {
+
+  }
+
+  getTodo(id): Promise<Todo> {
+
+  }
+
+  deleteTodo(todoToDelete): Promise<void> {
+
+  }
+
+  addTodo(title, description): Promise<void> {
+
+  }
 }
 
 export const Todos = new TodoController();
